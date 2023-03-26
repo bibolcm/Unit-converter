11b452e9feb4b1a8d31bfd4f76a59008b9fb2a51
Antonio <antoniocm_@hotmail.com>
2023-03-26 21:53:00 +0200

    volume, mass conversion added

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 
 const btnEl = document.getElementById("btn-el")
+const inputEl = document.getElementById("input-el")
+
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
 
 
-btnEl.addEventListener("click", function(){
-    alert("Hello")
+let inputValue = ""
+
+let metersToFeet
+let feetToMeters
+let litersToGallons
+let gallonsToLiters
+let kilosToPounds
+let poundsToKillos
+
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+btnEl.addEventListener("click", function () {
+
+    inputValue = inputEl.value
+
+    metersToFeet = (inputValue * 3.281).toFixed(3)
+    feetToMeters = (inputValue / 3.281).toFixed(3)
+
+    litersToGallons = (inputValue * 0.264).toFixed(3)
+    gallonsToLiters = (inputValue / 0.265).toFixed(3)
+
+    kilosToPounds = (inputValue * 2.204).toFixed(3)
+    poundsToKillos = (inputValue / 2.204).toFixed(3)
+
+    lengthEl.textContent = `${inputValue} meters = ${metersToFeet} feet | 20 feet = ${feetToMeters} meters`
+    volumeEl.textContent = `${inputValue} liters = ${litersToGallons} gallons | 20 gallons = ${gallonsToLiters} liters`
+    massEl.textContent = `${inputValue} kilos = ${kilosToPounds} pounds | 20 pounds = ${poundsToKillos} kilos`
+
+
+
+
+    console.log(inputEl.value)
 })
